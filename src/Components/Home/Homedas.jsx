a8ebf6bfaf576f7f6958cc2eb539aa1be13ff89c
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-04-13 21:36:25 -0400

    fdfsd

--- src/Components/Home/Homedas.jsx
+++ src/Components/Home/Homedas.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useAuth } from '../../context/AuthContext';
 
 export const HomeDas = () => {
 
+  const { user} = useAuth();
+
   return (
     <div className="containerHome">
-      <h1 className="welcomeMessage">Bienvenido:</h1>
+      <h1 className="welcomeMessage">Bienvenido: { user.nombre }</h1>
 
     </div>
   );
