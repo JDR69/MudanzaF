9c01ca95bc63abe4b78bde01527dd886c8e97527
Joan Dr <joandanielrr@gmail.com>
2025-05-08 10:02:03 -0400

    SDF

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,32 +2,39 @@ import instance from "./axios";
 
 const tiempoEspera = 10000;
 
+// LOGIN
 export const loginRequest = data => instance.post(`/api/auth/login`,data,{
     headers: {
         "Content-Type": "application/json"
       },
     withCredentials:true
 })
-
+// REGISTER CLIENTE
 export const registerReques = data => instance.post(`/api/auth/register`,data,{
     headers: {
         "Content-Type": "application/json"
       },
     withCredentials: true
 })
-
+// ROLES
 export const obtenerRolesRequest = () => { return instance.get(`/api/rol/get`, {
     withCredentials: true,
     timeout: tiempoEspera
 })}
 
-
+// BITACORA
 export const obtenerBitacoraRequest = () => { return instance.get(`/api/bitacora/get`,{
     withCredentials: true,
     timeout: tiempoEspera
 })}
-
-
+export const bitacoraVerificacionRequest = (data) => instance.post(`/api/bitacora/acceder`,
+    data,{
+        headers: {
+            "Content-Type": "application/json"
+          },
+        withCredentials:true
+    })
+// VEHICULO
 export const registerVehiculo = (data) => instance.post(`/api/veh/reg`,
     data,{
         headers: {
@@ -36,17 +43,45 @@ export const registerVehiculo = (data) => instance.post(`/api/veh/reg`,
         withCredentials:true
     }
 )
-
-export const obtenerTipoVehiculo = () => { return instance.get(`/api/tipoVeh/get`,{
-    withCredentials: true,
-    timeout: tiempoEspera
-})}
-
 export const obtenerVehiculo = () => { return instance.get(`/api/veh/get`,{
     withCredentials: true,
     timeout: tiempoEspera
 })}
 
+
+// TIPO VEHICULO
+export const obtenerTipoVehiculo = () => { return instance.get(`/api/tipoVeh/get`,{
+    withCredentials: true,
+    timeout: tiempoEspera
+})}
+
+export const tipoVehiculoRequest = (data) => instance.post( `/api/tipoVeh/reg`,
+    data,{
+        headers: {
+            "Content-Type": "application/json"
+          },
+        withCredentials:true
+    }
+)
+export const actualizarTipoVehiculo = (data,id) => instance.put(`/api/tipoVeh/upd/${id}`,
+    data,{
+        headers: {
+            "Content-Type": "application/json"
+          },
+        withCredentials:true
+    }
+)
+
+export const eliminarTipoVehiculo = (id) => instance.delete(`/api/tipoVeh/del/${id}`,
+    {
+        headers: {
+            "Content-Type": "application/json"
+          },
+        withCredentials:true
+    }
+)
+
+// CHOFER
 export const registerChofer = (data) => instance.post(`/api/chofer/reg`,
     data,{
         headers: {
@@ -89,34 +124,9 @@ export const eliminarConcecido = (data, id) =>
     });
   
 
-export const tipoVehiculoRequest = (data) => instance.post( `/api/tipoVeh/reg`,
-    data,{
-        headers: {
-            "Content-Type": "application/json"
-          },
-        withCredentials:true
-    }
-)
-
-export const actualizarTipoVehiculo = (data,id) => instance.put(`/api/tipoVeh/upd/${id}`,
-    data,{
-        headers: {
-            "Content-Type": "application/json"
-          },
-        withCredentials:true
-    }
-)
-
-export const eliminarTipoVehiculo = (id) => instance.delete(`/api/tipoVeh/del/${id}`,
-    {
-        headers: {
-            "Content-Type": "application/json"
-          },
-        withCredentials:true
-    }
-)
 
 
+// GALERIA DE VEHICULOS
 export const registrarGaleriaVehiculos = (data,id) => instance.post(`/api/veh/regImg/${id}`,
     data,{
         headers: {
@@ -125,13 +135,13 @@ export const registrarGaleriaVehiculos = (data,id) => instance.post(`/api/veh/re
         withCredentials:true
     }
 )
-
+// CATALOGO DE VEHICULOS
 export const obtenerCatalogoVehiculo = () => { return instance.get(`/api/veh/getCat`,{
     withCredentials: true,
     timeout: tiempoEspera
 })}
 
-
+// CATEGORIA DE INMUEBLES
 export const registrarCategoriaInmueble = (data) => instance.post(`/api/categorias/registrar`,
     data,{
         headers: {
@@ -166,7 +176,7 @@ export const eliminarCategoriaInmuebles = (id) => instance.delete(`/api/categori
         withCredentials:true
     }
 )
-
+// SEGUROS
 export const insertarNuevoSeguro = (data) => instance.post(`/api/seguro/reg`,
     data,{
         headers: {
@@ -198,7 +208,7 @@ export const eliminarSeguroBackend = (id) => instance.delete(`/api/seguro/del/${
         withCredentials:true
     }
 )
-
+// MATERIAL
 export const registrarMaterial = (data) => instance.post(`/api/materiales/registrar`,
     data,{
         headers: {
@@ -231,6 +241,7 @@ export const obtenerMateriales = () => { return instance.get(`/api/materiales/ge
     timeout: tiempoEspera
 })}
 
+// INMUEBLES
 export const registarInmueble = (data) => instance.post(`/api/inmuebles/registrar`,
     data,{
         headers: {
@@ -246,10 +257,3 @@ export const obtenerInmuebles = () => { return instance.get(`/api/inmuebles/get`
 })}
 
 
-export const bitacoraVerificacionRequest = (data) => instance.post(`/api/bitacora/acceder`,
-    data,{
-        headers: {
-            "Content-Type": "application/json"
-          },
-        withCredentials:true
-    })
