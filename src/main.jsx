c01c93efe9b352ce49a66054f189e7101cfc3236
BoteritoROOT <d.sajor2003rp@gmail.com>
2025-05-09 18:04:20 -0400

    as

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './Css/ComponentesComplets.css';
 import './Css/SelectsCompletsBoostrap.css';
